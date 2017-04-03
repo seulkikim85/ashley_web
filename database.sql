7f34a7db629c179f757371828bbe5a655e691cd2
Ghangseok <redeteus@gmail.com>
2017-04-03 14:21:23 +0900

    add basket create basket table product detail sql update

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE `Ashley`
 CREATE DATABASE `Ashley` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 CREATE TABLE `Orders` (
@@ -21,15 +22,15 @@ INSERT INTO `ashley`.`orders` (`order_id`, `email`, `product_id`, `qty`, `is_del
 
 CREATE TABLE `Products` (
   `product_id` varchar(10) COLLATE utf8_bin NOT NULL,
-  `brand_name` varchar(50) COLLATE utf8_bin DEFAULT NOT NULL,
-  `product_name` varchar(100) COLLATE utf8_bin DEFAULT NOT NULL,
-  `category` varchar(3) COLLATE utf8_bin DEFAULT NOT NULL,
-  `description` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
-  `img_url` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
-  `qty` int(11) DEFAULT NOT NULL,
-  `price` int(11) DEFAULT NOT NULL,
-  `create_dt` varchar(14) COLLATE utf8_bin DEFAULT NOT NULL,
-  `update_dt` varchar(14) COLLATE utf8_bin DEFAULT NOT NULL,
+  `brand_name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `product_name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `category` varchar(3) COLLATE utf8_bin NOT NULL,
+  `description` varchar(1000) COLLATE utf8_bin NULL,
+  `img_url` varchar(1000) COLLATE utf8_bin NULL,
+  `qty` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `create_dt` varchar(14) COLLATE utf8_bin NOT NULL,
+  `update_dt` varchar(14) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`product_id`)
 );
 
@@ -37,7 +38,20 @@ INSERT INTO `ashley`.`products` (`product_id`, `brand_name`, `product_name`, `ca
 INSERT INTO `ashley`.`products` (`product_id`, `brand_name`, `product_name`, `category`, `description`, `img_url`, `qty`, `price`, `create_dt`, `update_dt`) VALUES ('00010002', 'Apple', 'iphon7', '002', 'iphone7', './image/lipstick.jpg', '4', '1200', '20170320183400', '20170320183400');
 INSERT INTO `ashley`.`products` (`product_id`, `brand_name`, `product_name`, `category`, `description`, `img_url`, `qty`, `price`, `create_dt`, `update_dt`) VALUES ('00010003', 'Apple', 'iphon8', '003', 'iphone8', './image/lipstick.jpg', '3', '2000', '20170320183400', '20170320183400');
 INSERT INTO `ashley`.`products` (`product_id`, `brand_name`, `product_name`, `category`, `description`, `img_url`, `qty`, `price`, `create_dt`, `update_dt`) VALUES ('00010004', 'Dell', 'Dell inspire', '001', 'Dell inspire', './image/lipstick.jpg', '6', '2500', '20170320183400', '20170320183400');
-    
+
+CREATE  TABLE `ashley`.`MyBasket` (
+  `basket_id` INT NOT NULL AUTO_INCREMENT ,
+  `email` VARCHAR(100) NOT NULL ,
+  `product_id` VARCHAR(10) NOT NULL ,
+  `qty` INT NOT NULL ,
+  `is_ordered` VARCHAR(1) NOT NULL ,
+  `create_dt` VARCHAR(14) NOT NULL ,
+  `update_dt` VARCHAR(14) NULL ,
+  PRIMARY KEY (`basket_id`) )
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
 
 
 
--- product_detail.php
+++ product_detail.php
@@ -20,17 +20,27 @@
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $conn->prepare("SELECT product_id
-                                     , brand_name
-                                     , product_name
-                                     , category
-                                     , description
-                                     , img_url
-                                     , price
-                                     , qty
-                                     , update_dt
-                                    FROM Products
-                                   WHERE product_id = :product_id
+        $stmt = $conn->prepare("SELECT P.product_id
+                                     , P.brand_name
+                                     , P.product_name
+                                     , P.category
+                                     , P.description
+                                     , P.img_url
+                                     , P.price
+                                     , P.qty - IFNULL(O.qty, 0) as qty
+                                     , CASE P.qty - O.qty WHEN 0 THEN 'Y'
+                                       ELSE 'N'
+                                       END  as is_soldout
+                                     , P.update_dt
+                                    FROM Products P
+                                    LEFT JOIN 
+                                    (SELECT product_id
+                                          , sum(qty) as qty
+                                       FROM Orders
+                                      GROUP BY product_id) O
+                                      ON P.product_id = O.product_id
+                                   WHERE P.product_id = :product_id
+                                   ORDER BY P.update_dt DESC
                                ");
         $stmt->bindParam(':product_id', $product_id);
         $stmt->execute();
